51fc3cb4b65c77a84f7cef3c6c8db2636fd78c9d
Artem Pavlov <vizorcat@yandex.ru>
2025-01-07 23:41:50 +0300

    Приведение кода к единому стилю. Добавление позитивных проверок (get_book_genre). Добавление параметра self.

--- tests.py
+++ tests.py
@@ -17,7 +17,6 @@ class TestBooksCollector:
         collector.add_new_book("Гордость и предубеждение и зомби")
         collector.add_new_book("Что делать, если ваш кот хочет вас убить")
 
-        # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
